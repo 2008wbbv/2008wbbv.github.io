244defd24f10d434b1b23a251911e8d8fb9722af
benvaccaro <126634454+2008wbbv@users.noreply.github.com>
2024-08-15 15:34:47 +0000

    fixing js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const paragraphs = [
-        "This is the first random paragraph.",
-        "Here is another example of a random paragraph.",
-        "Random paragraphs are a fun way to show variety!",
-        "This text will change each time you refresh the page.",
-        "Every refresh brings a new message."
+        "Preoccupied with a single leaf... you won't see the tree. Preoccupied with a single tree... you'll miss the entire forest.",
+        "All truly strong people are kind.",
+        "A precipitous slope, a narrow river width, a fast and violent flow. Due to the terrain and the external influence, the state of the water is perfectly decided. And yet, water obeys only itself. Water is only water. Thoroughly water. Absolutely free.",
+        "My sword is one with heaven and earth. That's why in a fight, I need no sword",
+        "As the four seasons between Heaven and Earth flow in an endless cycle, so too, must man work his way through his own cycle of seasons continuous.",
+        "If there is a point to having been born then just take it all in. Each part born is by heaven perfectly decided and at once perfectly free.",
+        "Live on and endure the shadows! And brightness shall come your way.",
+        "Nothing goes perfectly for us. But... being incomplete is what pushes us onward to the next something... If we were even perfectly satisfied, what meaning would the rest of our lives hold, right?",
+        "Don't ever give up hope until the very last moment. If you give up, the game is already over."
+        
     ];
 
     const images = [
